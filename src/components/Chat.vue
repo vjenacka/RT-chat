ee6abccef5e37b8499556ff80dc94e22b7f77e69
vjenacka <edins613@gmail.com>
2019-12-12 00:33:07 +0100

    formated time, added scroll to the msges and added set the scroll to always be the new msg

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,10 +3,10 @@
     <h2 class="center teal-text">My Chat</h2>
     <div class="car">
       <div class="card-content">
-        <ul class="messages">
+        <ul class="messages" v-chat-scroll>
           <li v-for="message in messages" :key="message.id">
-            <span class="teal-text">{{message.name}} </span>
-            <span class="grey-text text-darken-3 msg-content">{{message.content}}</span>
+            <span class="teal-text">{{message.name}}</span>
+            <span class="grey-text text-darken-3 msg-content msg-content">{{message.content}}</span>
             <span class="grey-text time">{{message.timestamp}}</span>
           </li>
         </ul>
@@ -21,6 +21,7 @@
 <script>
 import NewMessage from "./NewMessage";
 import db from "../firebase/init";
+import moment from "moment";
 export default {
   name: "Chat",
   props: ["name"],
@@ -41,7 +42,7 @@ export default {
           obj.id = doc.id;
           obj.name = doc.data().name;
           obj.content = doc.data().content;
-          obj.timestamp = doc.data().timestamp;
+          obj.timestamp = moment(doc.data().timestamp).format("lll");
 
           this.messages.push(obj);
         }
@@ -53,6 +54,24 @@ export default {
 
 <style lang="scss" scoped>
 .chat {
+  .messages {
+    max-height: 300px;
+    overflow: auto;
+
+    &::-webkit-scrollbar {
+      width: 3px;
+    }
+    &::-webkit-scrollbar-track {
+      background: #ddd;
+    }
+    &::-webkit-scrollbar-thumb {
+      background: #aaa;
+    }
+
+    li {
+      padding: 5px 0;
+    }
+  }
   h2 {
     font-size: 2.6em;
     margin-bottom: 40px;
@@ -60,9 +79,12 @@ export default {
   span {
     font-size: 1.4em;
   }
+  .msg-content {
+    margin-left: 10px;
+  }
   .time {
     display: block;
-    font-size: 1.2em;
+    font-size: 0.9em;
   }
 }
 </style>
