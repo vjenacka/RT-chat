f35071ac283f6eef6452c607a76a4af274a0414e
vjenacka <edins613@gmail.com>
2019-12-12 00:17:29 +0100

    added db listener for adding new messages and outputing them in the component

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -4,10 +4,10 @@
     <div class="car">
       <div class="card-content">
         <ul class="messages">
-          <li>
-            <span class="teal-text">Name</span>
-            <span class="grey-text text-darken-3">message</span>
-            <span class="grey-text time">time</span>
+          <li v-for="message in messages" :key="message.id">
+            <span class="teal-text">{{message.name}} </span>
+            <span class="grey-text text-darken-3 msg-content">{{message.content}}</span>
+            <span class="grey-text time">{{message.timestamp}}</span>
           </li>
         </ul>
       </div>
@@ -20,12 +20,33 @@
 
 <script>
 import NewMessage from "./NewMessage";
+import db from "../firebase/init";
 export default {
   name: "Chat",
   props: ["name"],
   components: { NewMessage },
   data() {
-    return {};
+    return {
+      messages: []
+    };
+  },
+  created() {
+    let ref = db.collection("messages").orderBy("timestamp");
+    //referenca baze i onSnapshot prati promjene u bazi te dodaje obj kad je 'added'
+    ref.onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(change => {
+        const { doc } = change;
+        if (change.type === "added") {
+          let obj = {};
+          obj.id = doc.id;
+          obj.name = doc.data().name;
+          obj.content = doc.data().content;
+          obj.timestamp = doc.data().timestamp;
+
+          this.messages.push(obj);
+        }
+      });
+    });
   }
 };
 </script>
